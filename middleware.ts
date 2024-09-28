e738b9367d1683892e50d4a7603b30b21f535fd7
nipamrohit <nipamrohit121@gmail.com>
2024-09-28 22:24:50 +0530

    push3

--- middleware.ts
+++ middleware.ts
@@ -2,31 +2,11 @@ import { clerkMiddleware } from "@clerk/nextjs/server";
 
 
 export default clerkMiddleware({
-<<<<<<< HEAD
     // publicRoutes: [
-=======
-    // publicRoutes : [
->>>>>>> b1355de36654917d0c58b1645780d1decca18c6e
     //     '/',
     //     '/events/:id',
     //     '/api/webhook/clerk',
     //     '/api/webhook/stripe',
-<<<<<<< HEAD
-    //     '/api/uploadthing',
-    // ],
-    // ignoredRoutes: [
-    //     '/api/webhook/clerk',
-    //     '/api/webhook/stripe',
-    //     '/api/uploadthing',
-    // ],
-});
-
-export const config = {
-    matcher: [
-        '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-        '/(api|trpc)(.*)',
-    ],
-=======
     //     'api/uploadthing'
     // ],
     // ignoredRoutes : [
@@ -43,5 +23,4 @@ export const config = {
     // Always run for API routes
     '/(api|trpc)(.*)',
   ],
->>>>>>> b1355de36654917d0c58b1645780d1decca18c6e
 };
